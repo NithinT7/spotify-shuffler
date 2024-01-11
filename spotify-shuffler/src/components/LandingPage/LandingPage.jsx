6cf7af0a59fcdac45af6830d10c98c24f658c5ff
NithinT7 <90013428+NithinT7@users.noreply.github.com>
2024-01-10 19:51:21 -0800

    initial commit

---
+++ spotify-shuffler/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Login } from './Login'
+
+const LandingPage = () => {
+  return (
+    <div className='LandingPage'>
+        <h1>Shuffle Your Spotify</h1>
+        <Login/>
+    </div>
+  )
+}
+
+export default LandingPage
