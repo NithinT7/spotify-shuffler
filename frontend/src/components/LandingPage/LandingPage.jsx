bfd15e0c3d061ce1f97094346bf0c9f0baef2bfa
NithinT7 <90013428+NithinT7@users.noreply.github.com>
2024-01-15 21:53:27 -0800

    Update LandingPage.jsx

--- frontend/src/components/LandingPage/LandingPage.jsx
+++ frontend/src/components/LandingPage/LandingPage.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Login } from './Login'
-import logo from '../shufflplay-high-resolution-logo-transparent.png'
 
 const LandingPage = () => {
   return (
     <div className='w-full h-full flex flex-col font-sans justify-evenly items-center bg-spotifyBlack'>
-      <div className='w-full flex justify-center text-4xl text-spotifyGreen font-black md:text-6xl'>
-        <img src={logo} alt='ShufflPlay Logo' className='w-1/2 lg:w-1/4' />
+      <div className='w-full text-center text-4xl text-spotifyGreen font-black md:text-6xl'>
+        SHUFFLPLAY
       </div>
       <Login />
 
