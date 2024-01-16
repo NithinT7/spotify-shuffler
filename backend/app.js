ea48e8a127e50b5543342614833a318f6e06a142
NithinT7 <90013428+NithinT7@users.noreply.github.com>
2024-01-15 23:14:19 -0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 );
 
 
-const REDIRECT_URI = 'https:/shufflplay.com/shuffler'; // Your Redirect URI
+const REDIRECT_URI = 'https://shufflplay.com/shuffler'; // Your Redirect URI
 
 function generateRandomString(length) {
     let text = '';
